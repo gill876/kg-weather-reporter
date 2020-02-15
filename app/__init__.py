576ee9bf36b97c042a3347ec44905982dae77779
Cargill Seiveright <microcargill@hotmail.com>
2020-02-15 16:29:35 -0500

    Initial setup

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Som3$ec5etK*y'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cargill_db:password@localhost/krace_gennedy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.create_all()######
+
+mail = Mail(app)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+
+from app import views, models
---
+++ app/models.py
@@ -0,0 +1,26 @@
+from . import db
+
+class Worker(db.Model):
+
+    workerID = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    fname = db.Column(db.String(30))
+    lname = db.Column(db.String(30))
+    address1 = db.Column(db.String(100))
+    city = db.Column(db.String(30))
+    country = db.Column(db.String(30))
+    telephone = db.Column(db.String(20))
+    role = db.Column(db.String(100))
+    email = db.Column(db.String(50))
+
+    def __init__(self, fname, lname, address1, city, country, telephone, role, email):
+        self.fname = fname
+        self.lname = lname
+        self.address1 = address1
+        self.city = city
+        self.country = country
+        self.telephone = telephone
+        self.role = role
+        self.email = email
+
+    def __repr__(self):
+        return '%r, %r, %r, %r, %r, %r, %r, %r' % self.fname, self.lname, self.address1, self.city, self.country, self.telephone, self.role, self.email
